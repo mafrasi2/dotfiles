33df40b9e40b593bf16296001e600277e5bfe9ba
Maximilian Sistemich <maximilian.sistemich@rwth-aachen.de>
2017-08-24 15:36:27 +0200

    Added ssh-agent to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,12 @@
 set fish_greeting ""
+set PATH $PATH ~/.local/bin
 set -x EDITOR nvim
-set PATH "$HOME/.cargo/bin" $PATH
+
+
+if test -z "$SSH_ENV"
+    set -xg SSH_ENV $HOME/.ssh/environment
+end
+
+if not __ssh_agent_is_started
+    __ssh_agent_start
+end
