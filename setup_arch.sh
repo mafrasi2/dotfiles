f62d724715432414629a420d9fecb8a08515e995
Maximilian Sistemich <maximilian.sistemich@rwth-aachen.de>
2018-03-24 18:00:24 +0100

    fixed setup script for some containers

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-sudo pacman -Syy stow neovim python-pip fish tmux
+sudo pacman -Syy stow neovim python-pip fish tmux rxvt-unicode-terminfo
 pip install --user neovim
 
 stow nvim
 stow tmux
 stow fish
 
+printf "\nset -x TERM=rxvt-unicode-256color" >> ~/.config/fish/config.fish
 printf "\nexport TERM=rxvt-unicode-256color" >> ~/.bashrc
 
 nvim # download plugins and stuff
