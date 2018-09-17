f9e84468e2f7275b2caefcb8dea74a54c611c14e
Maximilian Sistemich <maximilian.sistemich@rwth-aachen.de>
2018-09-17 15:35:26 +0200

    fixed typo

--- setup_arch.sh
+++ setup_arch.sh
@@ -7,7 +7,7 @@ stow nvim
 stow tmux
 stow fish
 
-printf "\nset -x TERM=rxvt-unicode-256color" >> ~/.config/fish/config.fish
+printf "\nset -x TERM rxvt-unicode-256color" >> ~/.config/fish/config.fish
 printf "\nexport TERM=rxvt-unicode-256color" >> ~/.bashrc
 
 nvim # download plugins and stuff
