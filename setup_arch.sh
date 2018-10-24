a1a47fd98cf76b53de767d1c282ed3f74daac8be
Maximilian Sistemich <maximilian.sistemich@rwth-aachen.de>
2018-10-25 00:55:08 +0200

    added fisher and z-fish

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-sudo pacman -Syy stow neovim python-pip fish tmux rxvt-unicode-terminfo ripgrep fd
+sudo pacman -Syy stow neovim python-pip fish tmux rxvt-unicode-terminfo ripgrep fd curl
 pip install --user neovim
+
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
 stow nvim
 stow tmux
 stow fish
+
+fish -c "fisher add jethrokuan/z"
 
 printf "\nset -x TERM rxvt-unicode-256color" >> ~/.config/fish/config.fish
 printf "\nexport TERM=rxvt-unicode-256color" >> ~/.bashrc
